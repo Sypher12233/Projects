8f0f29b436ba0d85297e67474683e0e43b9a9d4f
Sypher <85158811+Sypher12233@users.noreply.github.com>
2022-01-20 15:46:23 +0430

    Year converter

---
+++ Dog to Human years.js
@@ -0,0 +1,16 @@
+const myName = 'HAPPY'.toLowerCase();
+
+const myAge = 8;//As you may know, 45 years in a human's life equals to 8 years in a dog's life.a dog's growth is much faster than a human's, hence we calculate dog's growth in 'dog years'.
+let earlyYears = 2;
+earlyYears *= 10.5;//Dog's first two years count as 10.5 dog years and after that each following year equals 4 dog years.
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
+/*TWITTER HANDLE: -------AS ALLWAYS, FEEL FREE TO DM ME ON TWITTER @uzairkhan123221 REGARDING ANY ISSUES :) ————*/
