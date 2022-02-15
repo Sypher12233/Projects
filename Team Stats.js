f87c2cef3ef6dcf1ebf4c4387596df5f63b189d6
Sypher <85158811+Sypher12233@users.noreply.github.com>
2022-02-15 12:52:40 +0430

    project finished

--- Team Stats.js
+++ Team Stats.js
@@ -19,17 +19,17 @@ const team = {
       opponent: 'Pakistan',
       teamScore: 100,
       opponentScore: 80
-      },
-       { 
-         opponent: 'India',
-         teamScore: 90,
-         opponentScore: 50,
-        },
-         { 
-          opponent: 'Srilanka',
-          teamScore: 110,
-          opponentScore: 70
-          }
+    },
+    { 
+    opponent: 'India',
+    teamScore: 90,
+    opponentScore: 50,
+    },
+    { 
+    opponent: 'Sri Lanka',
+    teamScore: 110,
+    opponentScore: 70
+    }
   ],
   get players() {
     return this._players;
@@ -54,15 +54,23 @@ const team = {
     return this.games.push(game);
   },
 };
-
+//add the players below as you wish.
 team.addPlayer('Steph', 'Curry', 28);
 team.addPlayer('Lisa', 'Leslie', 44);
 team.addPlayer('Bugs', 'Bunny', 76);
 
+//add the opponent teams name and score below.
 team.addGame('England', 90, 50);
 team.addGame('Australia', 40, 90);
 team.addGame('USA', 60, 70);
 
 
+//check your results.
+console.log(team.players);
+console.log(team.games);
 
-console.log(team.players, team.games);
+
+
+/* TWITTER HANDLE: 
+    -------AS ALLWAYS, FEEL FREE TO DM ME ON TWITTER @uzairkhan123221 REGARDING ANY ISSUES :) ————
+*/
